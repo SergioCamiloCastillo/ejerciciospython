227a9b4d34105cb893adb8c4061769c1ad3af14b
sergio castillo <scastillonu@uniminuto.edu.co>
2020-10-09 21:38:19 -0500

    second change

--- ejercicios.py
+++ ejercicios.py
@@ -1,3 +1,9 @@
+"""
+Crear un módulo que solicite al usuario el ingreso de un nombre de usuario y contraseña y
+que los valide utilizando los módulos generados en los dos ejercicios anteriores.
+Ayuda: para contar la cantidad de caracteres de una cadena, en Python se utiliza la
+función incorporada: len(cadena)
+"""
 import usuario
 import passw
 
--- passw.py
+++ passw.py
@@ -1,3 +1,16 @@
+"""
+Crear un módulo para validación de contraseñas. Dicho módulo, deberá cumplir con
+los siguientes criterios de aceptación:
+• La contraseña debe contener un mínimo de 8 caracteres
+• Una contraseña debe contener letras minúsculas, mayúsculas, números y al menos
+1 carácter no alfanumérico
+• La contraseña no puede contener espacios en blanco
+• Contraseña válida, retorna True
+• Contraseña no válida, retorna el mensaje “La contraseña elegida no es segura”
+
+
+"""
+
 def clave(password):
 
         validar=False #que se vayan cumpliendo los requisitos uno a uno.
